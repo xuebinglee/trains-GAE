1a41dfd672c0e8103ca64fa9f131d30a88a6a62c
Xuebing Li <xuebinglee@gmail.com>
2013-06-26 19:39:02 +1000

    Graph done. More testing needed.

---
+++ Graph/Graph.py
@@ -0,0 +1,37 @@
+from Town import Town
+
+
+class Graph():
+    """Graph"""
+    def __init__(self):
+        self.__towns = []
+
+    def addTown(self, townID):
+        t = Town(townID)
+        self.__towns.append(t)
+
+    def deleteTown(self, townID):
+        if townID not in self.__towns:
+            raise Exception('Town does not exist.')
+        self.__towns.remove(townID)
+
+    def updateEdge(self, originID, destinationID, distance):
+        if originID not in self.__towns:
+            raise Exception('Origin does not exist.')
+        if destinationID not in self.__towns:
+            raise Exception('Destination does not exist.')
+        if distance <= 0:
+            raise Exception('Distance must be non-negative.')
+        origin = next(town for town in self.__towns if town == originID)
+        origin.updateEdge(destinationID, distance)
+
+    def deleteEdge(self, originID, destinationID):
+        if originID in self.__towns:
+            origin = next(t for t in self.__towns if t == originID)
+            origin.deleteEdge(destinationID)
+
+    def __repr__(self):
+        rep = ''
+        for town in self.__towns:
+            rep += str(town)
+        return rep
---
+++ Graph/Town.py
@@ -0,0 +1,51 @@
+class Town():
+    """Town"""
+    def __init__(self, id):
+        self.__id = id
+        self.__edges = {}
+
+    def updateEdge(self, destinationID, distance):
+        self.__edges[destinationID] = distance
+
+    def deleteEdge(self, destinationID):
+        if destinationID not in self.__edges.values():
+            raise Exception('The to-be-deleted edge does not exist.')
+        del self.__edges[destinationID]
+
+    # getters
+    def id(self):
+        return self.__id
+
+    def isConnectTo(self, destinationID):
+        return destinationID in self.__edges.keys()
+
+    def distanceTo(self, destinationID):
+        return self.__edges[destinationID]
+
+    def connectedTowns(self):
+        return self.__edges.keys()
+
+    def __repr__(self):
+        rep = 'Town: ' + self.__id + '\n'
+        for destinationID, distance in self.__edges.iteritems():
+            rep += '      ->' + destinationID + ' ' + str(distance) + '\n'
+        return rep
+
+    # operator overloading
+    # ==
+    def __eq__(self, other):
+        if isinstance(other, basestring):
+            return self.__id == other
+        elif isinstance(other, Town):
+            return self.__id == other.__id
+        else:
+            raise Exception('Can only compare Town to another Town or an ID.')
+
+    # !=
+    def __ne__(self, other):
+        if isinstance(other, basestring):
+            return self.__id != other
+        elif isinstance(other, Town):
+            return self.__id != other.__id
+        else:
+            raise Exception('Can only compare Town to another Town or an ID.')
