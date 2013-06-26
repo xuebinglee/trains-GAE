94b184be5d50534c4a21b14bc5eec23679913623
Xuebing Li <xuebinglee@gmail.com>
2013-06-26 21:15:52 +1000

    framework changed to bottle

--- trains.py
+++ trains.py
@@ -1,24 +1,32 @@
 import os
+import bottle
+from bottle import route, request, error, debug
 
-import webapp2
 import jinja2
+from google.appengine.ext.webapp.util import run_wsgi_app
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
 
 
-class IndexPage(webapp2.RequestHandler):
-
-    def get(self):
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render({}))
-
-    def post(self):
-        post_request = self.request.body
-        self.response.write(post_request)
+@route('/')
+def App():
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    return template.render({})
 
 
-application = webapp2.WSGIApplication([
-    ('/', IndexPage),
-], debug=True)
+@route('/AddTown', method='POST')
+def AddTown():
+    post_request = request.body.read()
+    return post_request
+
+
+@error(404)
+def Error404(code):
+    return 'Sorry, we cannot find what you\'re looking for..'
+
+
+debug(True)
+run_wsgi_app(bottle.default_app())
